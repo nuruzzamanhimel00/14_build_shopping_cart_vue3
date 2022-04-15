9df927d2ebf0c9fd80bab7338d6ab9fe2240d7c4
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-15 21:32:21 +0600

    navbar is active

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
  <div>
-    <div class="navbar">
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <router-link :to="{ name: 'HomeComponent'}" class="nav-link">Home</router-link>
-        </li>
-         <li class="nav-item">
-          <router-link :to="{ name: 'AboutComponent'}" class="nav-link">About</router-link>
-        </li>
-      </ul>
-    </div>
+  
     <div class="container">
+      <div class="navbar">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <router-link :to="{ name: 'HomeComponent'}" class="nav-link"
+              :class="[$route.name == 'HomeComponent' ? 'active' : '']"
+            >Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'AboutComponent'}" class="nav-link"
+             :class="[$route.name == 'AboutComponent' ? 'active' : '']"
+            >About</router-link>
+          </li>
+        </ul>
+      </div>
+        {{ $route.name }}
       <router-view></router-view>
     </div>
  </div>
 </template>
+
+<style lang="css">
+  html, body{
+    margin: 0px;
+    padding: 0px;
+    overflow-x: hidden ;
+  }
+  .nav-item a.nav-link{
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+  }
+  .nav-item a.nav-link.active{
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+    background: red none repeat scroll 0% 0%;
+    background-color: red;
+    background-image: none;
+    color: #fff;
+    border-radius: 7px;
+    --darkreader-inline-bgcolor: #cc0000;
+    --darkreader-inline-bgimage: none;
+    --darkreader-inline-color: #e8e6e3;
+  }
+</style>
 
 
